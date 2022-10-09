430f5aed9da4753f36cb35d4209f2fddda6b8bba
cristinareguero <crisreguemedin@gmail.com>
2022-10-09 20:59:23 +0200

    lo he guardado

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+'''
+Ejercicio 2
+
+Utilizando todo lo que sabes sobre cadenas, listas, sus métodos internos... Transforma este texto:
+un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro
+En este otro:
+Un día que el viento soplaba con fuerza...
+- Mira como se mueve aquella banderola -dijo un monje.
+- Lo que se mueve es el viento -respondió otro monje.
+- Ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro.
+Lo único prohibido es modificar directamente el texto.
+
+'''
