b1f0875338d5b17ba8c034f4ad287af3b7b4941d
cristinareguero <crisreguemedin@gmail.com>
2022-10-09 21:42:46 +0200

    he cambiado cosas

--- ejercicio2.py
+++ ejercicio2.py
@@ -14,14 +14,28 @@ Lo único prohibido es modificar directamente el texto.
 
 texto = "un día que el viento soplaba con fuerza#mira como se mueve aquella bandolera -dijo un monje#lo que es el viento -respondió otro monje#ni las bandoleras ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
 
+texto_separado = texto.split("#")
+
+for i in texto_separado:
+    mayuscula = i.capitalize()
+    print(mayuscula, end = "\n")
+    #print(texto_separado[i])
+
+# Poner primera letra de cada frase en mayúscula.
+#mayuscula = [i.capitalize() for i in texto_separado]
+#print(mayuscula, end = "\n")
+
+
+
 # Forma 1 eliminar #:
 
-caracter_eliminar = "#"
-texto = "".join(x for x in texto if x not in caracter_eliminar)
-print(texto)
+# caracter_eliminar = "#"
+#texto = "".join(x for x in texto if x not in caracter_eliminar)
+#print(texto)
 
 # Forma 2 eliminar #:
 
-for x in range(len(caracter_eliminar)):
-    texto = texto.replace(caracter_eliminar[x], "")
-print(texto)
+#for x in range(len(caracter_eliminar)):
+    #texto = texto.replace(caracter_eliminar[x], "")
+#print(texto)
+
